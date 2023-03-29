6a4ab68e40f3f36d9a8b366ee977806aa2093e0f
Elysia_firstsunshine <1597123764@qq.com>
2023-03-29 15:16:45 +0800

    1.0.02

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
-const Layout = ({handleClcik}) => {
+const Layout = () => {
   return (
     <div className="App">
-      <Navbar handleClcik={handleClcik}/>
+      <Navbar/>
       <Outlet />
     </div>
   );
